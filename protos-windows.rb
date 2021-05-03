f250bbfb4fae6203ae510de2667c8f6ee7b3a0c5
Daniel Azuma <dazuma@google.com>
2021-05-03 18:23:13 +0000

    Protos on Windows test

--- protos-windows.rb
+++ protos-windows.rb
@@ -4,27 +4,44 @@ Google::Protobuf::DescriptorPool.generated_pool.build do
   add_file("windoze.proto", :syntax => :proto3) do
     add_message "windoze.SimpleTest" do
       optional :foo, :string, 1
-      optional :bar, :string, 2
-    end
-    add_message "windoze.MessageTest" do
-      optional :foo, :message, 1, "windoze.SimpleTest"
-      optional :bar, :string, 2
-    end
-    add_message "windoze.RepeatedTest" do
-      repeated :foo, :string, 1
-      optional :bar, :string, 2
     end
   end
 end
 module Windoze
-  MessageTest = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("windoze.MessageTest").msgclass
-  RepeatedTest = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("windoze.RepeatedTest").msgclass
   SimpleTest = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("windoze.SimpleTest").msgclass
 end
 
-obj = Windoze::SimpleTest.new({})
-p obj
-obj = Windoze::MessageTest.new({})
-p obj
-obj = Windoze::RepeatedTest.new({})
-p obj
+$failed = false
+
+def test(name)
+  puts "**** STARTING: #{name} ****"
+  yield
+  puts "**** SUCCESS: #{name} ****"
+rescue => e
+  puts e.to_s
+  puts e.backtrace.join("\n")
+  $failed = true
+end
+
+test("to_s") do
+  message = Windoze::SimpleTest.new
+  $result = message.to_s
+  p $result
+end
+
+test("hash2") do
+  (21..40).each do |i|
+    p (i*i*16337).to_s.hash.to_s(16)
+  end
+  p Object.new.hash.to_s(16)
+end
+
+test("hash") do
+  (21..40).each do |i|
+    message = Windoze::SimpleTest.new({foo: (i*i*16337).to_s})
+    p message.hash.to_s(16)
+  end
+end
+
+
+abort if $failed
